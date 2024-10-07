36c050d895c9015e09f148c30338c196f932ab4f
jdfinch <jdfinch@emory.edu>
2024-10-07 12:56:32 -0400

    Data processing stubs

---
+++ dsi/data/multiwoz24/convert_to_tspy.py
@@ -0,0 +1,7 @@
+
+
+# load raw version of data
+
+# create a DSTData object with all the correct linking
+
+# call DSTData.save() to save the data to a new location
--- dsi/data/structure.py
+++ dsi/data/structure.py
@@ -1,6 +1,7 @@
 
 import dataclasses as dc
 import ezpyzy as ez
+import pathlib as pl
 
 
 @dc.dataclass
@@ -61,7 +62,40 @@ class SlotValue:
 
 @dc.dataclass
 class DSTData:
-    ...
+    path: str = None
+    dialogues: dict[str, Dialogue] = None
+    turns: dict[tuple[str, int], Turn] = None  # (dialogue_id, index)
+    slots: dict[tuple[str, str], Slot] = None  # (name, domain)
+    slot_values: dict[tuple[str, int, str], SlotValue] = None # (dialogue_id, turn_index, slot_name)
+    
+    def __post_init__(self):
+        if self.path is not None:
+            turn_table = ez.TSPy.load(self.path)
+            dialogue_table = ez.TSPy.load(self.path)
+            slot_table = ez.TSPy.load(self.path)
+            slot_value_table = ez.TSPy.load(self.path)
+        ... # given these these tables, convert into the actual objects and link up all the refs
+
+    
+    def save(self):
+        
+        """
+        Create a list of lists for each table
+        Cell values all need to be python literals
+        """
+
+        turns = [[] for turn in self.turns.values()] # this is a stub
+        dialogues = ...
+        slots = ...
+        slot_values = ...
+
+        ez.TSPy.save(turns, pl.Path(self.path) / 'turns.tspy')
+        ez.TSPy.save(dialogues, pl.Path(self.path) / 'dialogues.tspy')
+        ez.TSPy.save(slots, pl.Path(self.path) / 'slots.tspy')
+        ez.TSPy.save(slot_values, pl.Path(self.path) / 'slot_values.tspy')
+
+
+
 
 
 
